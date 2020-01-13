a4e5e50d862f1c201bdbe8cfb039a66724a59ae0
Shoma KYAN <e175773@ie.u-ryukyu.ac.jp>
2020-01-13 22:11:00 +0900

    ・入力した数値が重複していると、もう一度入力させる処理を追加 ・答えの配列と入力した配列の比較

--- src/main/java/jp/ac/uryukyu/ie/e175773/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e175773/Main.java
@@ -22,13 +22,32 @@ public class Main {
         System.out.println("4つの異なる10進数を一つずつ入力して下さい");
         Scanner sc = new Scanner(System.in);
         int array[] = new int[4];
-        for(int i=0; i<4; i++) {
-           array[i] = sc.nextInt();
+        for(int i=0; i<array.length; i++) {
+            Loop: while(true) {
+                System.out.println((i+1)+"番目の数値を入力して下さい");
+                array[i] = sc.nextInt();  //0~9の数値を入れる
+                for (int j = 0; j < i; j++) {
+                    //その前までの数値のどれかとかぶっていたら数値代入からやり直し
+                    if (array[j] == array[i]) {
+                        System.out.println("数字が重複しています！もう一度"+(i+1)+"番目から入力し直して下さい！");
+                        continue Loop;
+                    }
+                }
+                break;
+            }
         }
         System.out.println();
-        for(int i=0; i<4; i++) {
-            System.out.print(array[i]+" ");
-        }
+        System.out.println(Arrays.toString(array));
 
+        int eat_number=0;
+        int bite_number=0;
+        for(int i=0; i<array.length; i++) {
+            for(int j=0; j<array.length; j++){
+                if(array[i] == answer[j] && i==j){eat_number=eat_number+1;}
+                if(array[i] == answer[j]){bite_number=bite_number+1;}
+            }
+        }
+        System.out.println("数字も場所も一致: "+eat_number);
+        System.out.println("数字のみ一致: "+bite_number);
     }
 }
