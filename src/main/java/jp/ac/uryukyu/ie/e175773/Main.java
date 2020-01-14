e2f5fb12f58c06898be3ad479b8da05b9dc52276
Shoma KYAN <e175773@ie.u-ryukyu.ac.jp>
2020-01-14 14:47:11 +0900

    ゲーム説明、例外処理を追加

--- src/main/java/jp/ac/uryukyu/ie/e175773/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e175773/Main.java
@@ -20,19 +20,41 @@ public class Main {
             }
         }
 
+        System.out.println("〜〜〜〜〜〜〜〜数当てゲーム〜〜〜〜〜〜〜〜〜〜");
+        System.out.println("ルール説明");
+        System.out.println("1. ランダムで用意したそれぞれ異なる3つの数字とそれぞれの順番を当てるゲームです。数字だけ全部当たっていても順番が異なっているとクリアできません。");
+        System.out.println("2. 数字は0~9の10進数で1番目から順番に入力します。例：「345」などまとめて入れることは不可能です。「3」「4」「5」と入力していきましょう。");
+        System.out.println("3. それぞれ異なる数字なので「3」「3」「4」などの一つでも重複する数字は入力できません");
+        System.out.println("4. 入力した数字とランダムで用意した数字を比較していきます。");
+        System.out.println("　　数字と順番両方とも一致している個数(EAT)と数字のみが一致している個数(BITE)を表示します。");
+        System.out.println("　　例：ランダムで用意された数字「3」「4」「5」に対して入力「1」「4」「3」だと、EAT:1-BITE:2となります。");
+        System.out.println("5. EAT:3 になればクリアです。");
+        System.out.println("それではプレイしていきましょう！めざせ１発クリア！(キツい)");
+        System.out.println();
         //数が当たるまで入力を繰り返す
         while (true) {
             count++;
             // 4つずつ数値を入力する。当たるまで繰り返す
             System.out.println("3つの異なる10進数を一つずつ入力して下さい");
             System.out.println("-------------"+count+"回目の入力-------------------");
-            Scanner sc = new Scanner(System.in);
             int array[] = new int[3];
             for (int i = 0; i < array.length; i++) {
                 Loop:
                 while (true) {
-                    System.out.println((i + 1) + "番目の数値を入力して下さい");
-                    array[i] = sc.nextInt();  //0~9の数値を入れる
+                        try{
+                            Scanner sc = new Scanner(System.in);
+                            System.out.println((i + 1) + "番目の数値を入力して下さい");
+                            array[i] = sc.nextInt();  //0~9の数値を入れる
+                        }catch(InputMismatchException e){
+                            System.out.println("入力が数値ではありません！");
+                            System.out.println((i + 1) + "番目から入力し直して下さい！");
+                            System.out.println();
+                            continue Loop;
+                        }
+                    if (array[i]>=10){
+                        System.out.println("数字が2桁以上です！" + (i + 1) + "番目から入力し直して下さい！");
+                        continue Loop;
+                    }
                     for (int j = 0; j < i; j++) {
                         //その前までの数値のどれかとかぶっていたら数値代入からやり直し
                         if (array[j] == array[i]) {
@@ -58,8 +80,8 @@ public class Main {
                     }
                 }
             }
-            System.out.println("数字も場所も一致(EAT): " + eat_number);
-            System.out.println("数字のみ一致(BITE): " + bite_number);
+            System.out.println("EAT (数字と順番両方とも一致している個数): " + eat_number);
+            System.out.println("BITE(数字のみが一致している個数)       : " + bite_number);
             System.out.println();
             if(eat_number==array.length){
                     System.out.println("正解です！");
